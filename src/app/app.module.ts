962af6d72944182d283dc456f629cbb0f937548e
Eden Liu <eden90267@gmail.com>
2018-08-23 17:28:44 +0800

    Impl...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {LayoutModule} from './layout/layout.module';
+import {LoginModule} from './login/login.module';
+import {PostsModule} from './posts/posts.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule,
+    LoginModule,
+    PostsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/login.module.ts
@@ -0,0 +1,13 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {AuthComponent} from './auth/auth.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [AuthComponent],
+  exports: [AuthComponent]
+})
+export class LoginModule {
+}
